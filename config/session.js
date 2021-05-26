6df218a311d2e84d953fd6e22f23beb896360c3e
Javier Perez <javier21perez98@gmail.com>
2021-05-26 09:47:59 -0700

    required .env file for sessions cookies connection to local mongodb

--- config/session.js
+++ config/session.js
@@ -1,27 +1,30 @@
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+require("dotenv").config();
 
 const { NODE_ENV, SESSION_SECRET, MONGODB_URI } = process.env;
 
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: "sessions",
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	uri: MONGODB_URI,
+	collection: "sessions",
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 store.on("error", (error) => console.error(error));
 
 // export config for session middleware
 module.exports = session({
-  secret: SESSION_SECRET,
-  cookie: {
-    sameSite: true,
-    // Don't use secure: true in development. Must use https with secure: true.
-    secure: NODE_ENV === "production",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-  },
-  resave: false,
-  saveUninitialized: false,
-  store,
+	secret: SESSION_SECRET,
+	cookie: {
+		sameSite: true,
+		// Don't use secure: true in development. Must use https with secure: true.
+		secure: NODE_ENV === "production",
+		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+	},
+	resave: false,
+	saveUninitialized: false,
+	store,
 });
