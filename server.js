1975b0f52a4720decd70b22e92ea5e7d3074718a
Javier Perez <javier21perez98@gmail.com>
2021-05-26 09:46:07 -0700

    configuring for connection to local mongodb

--- server.js
+++ server.js
@@ -11,26 +11,26 @@ checkEnv();
 const PORT = process.env.PORT || 3001;
 
 (async () => {
-  try {
-    await connectDb();
-    console.log(`🔌 MongoDB Connected`);
+	try {
+		await connectDb();
+		console.log(`🔌 MongoDB Connected`);
 
-    const app = express();
+		const app = express();
 
-    // trust proxy required for using secure cookies on Heroku
-    app.set("trust proxy", 1);
+		// trust proxy required for using secure cookies on Heroku
+		app.set("trust proxy", 1);
 
-    app.use(
-      express.urlencoded({ extended: true }),
-      express.json(),
-      session,
-      router,
-      errorMiddleware
-    );
-    app.listen(PORT, () => {
-      console.log(`🌎 ==> Server listening on port ${PORT}!`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+		app.use(
+			express.urlencoded({ extended: true }),
+			express.json(),
+			session,
+			router,
+			errorMiddleware
+		);
+		app.listen(PORT, () => {
+			console.log(`🌎 ==> Server listening on port ${PORT}!`);
+		});
+	} catch (error) {
+		console.error(error);
+	}
 })();
