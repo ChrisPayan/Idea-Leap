e513237d3ec911a3567d6c680a277f45e1427360
Javier Perez <javier21perez98@gmail.com>
2021-05-27 14:49:45 -0700

    imported modal for idea page

--- client/src/components/IdeaPage/index.js
+++ client/src/components/IdeaPage/index.js
@@ -2,24 +2,25 @@ import { useHistory } from "react-router-dom";
 import { useAuth } from "../../util/auth";
 import { Directions } from "../Directions/Directions";
 import ReactBeautifulDND from "../ReactBeautifulDND";
+import Modal from "../Modal";
 function IdeaPage() {
-  const history = useHistory();
-  const auth = useAuth();
+	const history = useHistory();
+	const auth = useAuth();
 
-  return (
-    <>
-      <Directions />
-      <ReactBeautifulDND />
-
-      {/* hide actions if user is logged in */}
-      {!auth.isLoggedIn() && (
-        <>
-          <button onClick={() => history.push("/login")}></button>
-          <button onClick={() => history.push("/signup")}></button>
-        </>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Directions />
+			<ReactBeautifulDND />
+			<Modal />
+			{/* hide actions if user is logged in */}
+			{!auth.isLoggedIn() && (
+				<>
+					<button onClick={() => history.push("/login")}></button>
+					<button onClick={() => history.push("/signup")}></button>
+				</>
+			)}
+		</>
+	);
 }
 
 export default IdeaPage;
