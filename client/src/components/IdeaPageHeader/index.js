828ba43f8d991e5e543e7057cc12dc56557f2215
Tyler Abegg <tafimba@gmail.com>
2021-05-28 14:02:20 -0700

    UDeleting comments

--- client/src/components/IdeaPageHeader/index.js
+++ client/src/components/IdeaPageHeader/index.js
@@ -5,23 +5,6 @@ function IdeaPageHeader() {
 		<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
 			<div className="max-w-xl sm:mx-auto lg:max-w-2xl">
 				<div className="flex flex-col mb-16 sm:text-center sm:mb-0">
-					{/* <a href="/" className="mb-6 sm:mx-auto">
-            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
-              <svg
-                className="w-10 h-10 text-deep-purple-accent-400"
-                stroke="currentColor"
-                viewBox="0 0 52 52"
-              >
-                <polygon
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  fill="none"
-                  points="29 13 14 29 25 29 23 39 38 23 27 23"
-                />
-              </svg>
-            </div>
-          </a> */}
 					<div className="flex items-center justify-center py-16">
 						<img src={brainstormImg} alt="still not displaying" />
 					</div>
--- client/src/components/Navbar/NavBar1.js
+++ client/src/components/Navbar/NavBar1.js
@@ -16,21 +16,6 @@ function Navbar1() {
             <div>
             <img src={ideaLoaderIcon} alt="ideaLoaderIcon" className="transform scale-75"></img>
             </div>
-            {/* <svg
-              className="w-8 text-teal-accent-400"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              stroke="currentColor"
-              fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12" />
-            </svg> */}
             <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
               IdeaLoader
             </span>
