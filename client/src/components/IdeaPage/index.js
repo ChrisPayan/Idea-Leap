f6e8370db8bad4b141e7af920d3861d7ae078555
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-30 04:53:29 -0700

    lifted state to display modal for random words

--- client/src/components/IdeaPage/index.js
+++ client/src/components/IdeaPage/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../../util/auth";
 import ReactBeautifulDND from "../ReactBeautifulDND";
@@ -7,28 +8,29 @@ import ResetButton from "../ResetButton";
 import IdeaPageTips from "../IdeaPageTips";
 
 function IdeaPage() {
-	const history = useHistory();
-	const auth = useAuth();
+  const [open, setOpen] = useState(true);
+  const history = useHistory();
+  const auth = useAuth();
 
-	return (
-		<>
-			<div className="items-center">
-				<IdeaPageHeader />
-				<ReactBeautifulDND />
-				<ResetButton />
-				<IdeaPageTips />
-				<Modal />
+  return (
+    <>
+      <div className="items-center">
+        <IdeaPageHeader onClick={() => setOpen(true)}/>
+        <ReactBeautifulDND />
+        <ResetButton />
+        <IdeaPageTips />
+        <Modal show={open} onClick={() => setOpen(false)} />
 
-				{/* hide actions if user is logged in */}
-				{!auth.isLoggedIn() && (
-					<>
-						<button onClick={() => history.push("/login")}></button>
-						<button onClick={() => history.push("/signup")}></button>
-					</>
-				)}
-			</div>
-		</>
-	);
+        {/* hide actions if user is logged in */}
+        {!auth.isLoggedIn() && (
+          <>
+            <button onClick={() => history.push("/login")}></button>
+            <button onClick={() => history.push("/signup")}></button>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default IdeaPage;
